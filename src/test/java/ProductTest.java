bb55f2b0f0f346e5cc5180580e660e628cba79e4
Oqsanna <oqsannas@localexpress.io>
2022-09-08 16:02:37 +0400

    Add ProductTest Delete CreateProductTest.java and DeleteProductTest.java

--- src/test/java/ProductTest.java
+++ src/test/java/ProductTest.java
@@ -5,12 +5,13 @@ import io.restassured.http.Header;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import responses.deleteProduct.DeleteProduct;
 import responses.deleteProduct.SuccessCreateProduct;
 import steps.data.users.UserInfoProvider;
 import storage.APIV2;
 import storage.USER;
 
-public class CreateProductTest {
+public class ProductTest {
     Gson gson = new Gson();
 
     @Test
@@ -29,5 +30,18 @@ public class CreateProductTest {
         Assertions.assertEquals("success", successCreateProduct.getResult());
         Assertions.assertEquals(200, successCreateProduct.getCode());
         Assertions.assertFalse(successCreateProduct.getData().isEmpty(), "data  is empty");
+    }
+
+    @Test
+    @DisplayName("Check delete product")
+    public void deleteProductTest() {
+        DeleteProduct deleteProductTest = RestAssured.given()
+                .header(new Header("Authorization", "Bearer " + UserInfoProvider.getToken()))
+                .delete(APIV2.STAGE.getApi() + APIV2.DELETE_PRODUCT_ID.getApi() + UserInfoProvider.getProductId())
+                .then().log().all()
+                .extract().as(DeleteProduct.class);
+        Assertions.assertEquals("success", deleteProductTest.getResult());
+        Assertions.assertEquals(200, deleteProductTest.getCode());
+        Assertions.assertTrue(deleteProductTest.isData());
     }
 }
