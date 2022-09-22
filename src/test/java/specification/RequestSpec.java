3c6cd243caa9b173ffa67a8ebc7e5ff1469ed383
Artem Sarkisian <artems@localexpress.io>
2022-09-22 15:16:08 +0400

    Specifications

--- src/test/java/specification/RequestSpec.java
+++ src/test/java/specification/RequestSpec.java
@@ -1,4 +1,4 @@
-package spec;
+package specification;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
@@ -7,23 +7,11 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-public class Specifications {
+public class RequestSpec {
     public static RequestSpecification requestSpec(String url) {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
-                .build();
-    }
-
-    public static ResponseSpecification responseOK200() {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(200)
-                .build();
-    }
-
-    public static ResponseSpecification responseError400() {
-        return new ResponseSpecBuilder()
-                .expectStatusCode(400)
                 .build();
     }
 
