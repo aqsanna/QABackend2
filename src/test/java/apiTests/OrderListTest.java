b94811804372deffd9041ca21c8a7d08cf4d757b
Artem Sarkisian <artems@localexpress.io>
2022-11-01 14:52:15 +0400

    format fix

--- src/test/java/apiTests/OrderListTest.java
+++ src/test/java/apiTests/OrderListTest.java
@@ -13,55 +13,50 @@ import storage.ORDER_STATUS;
 
 public class OrderListTest {
     UserInfoProvider userInfoProvider = new UserInfoProvider();
+
     @Test
     @DisplayName("Check partner order list")
-    public void orderListTest(){
+    public void orderListTest() {
         for (int i = 0; i < ORDER_STATUS.values().length; i++) {
             boolean emptyData = false;
             int limit = 20;
             int offset = 0;
+
             while (!emptyData) {
-                PartnerOrders orderList = RestAssured.given()
-                        .header(new Header("Authorization", "Bearer " + userInfoProvider.getToken()))
-                        .queryParam("filter[status]", ORDER_STATUS.values()[i].getOrderStatus())
-                        .queryParam("limit", limit)
-                        .queryParam("offset", offset)
-                        .queryParam("sort", "asc")
-                        .when()
-                        .get(APIV1.STAGE.getApi() + APIV1.ORDERS.getApi())
-                        .then()
-                        .extract().as(PartnerOrders.class);
+                PartnerOrders orderList = RestAssured.given().header(new Header("Authorization", "Bearer " + userInfoProvider.getToken())).queryParam("filter[status]", ORDER_STATUS.values()[i].getOrderStatus()).queryParam("limit", limit).queryParam("offset", offset).queryParam("sort", "asc").when().get(APIV1.STAGE.getApi() + APIV1.ORDERS.getApi()).then().extract().as(PartnerOrders.class);
+
                 Assertions.assertEquals("success", orderList.getResult(), "Have a error: " + orderList.getResult());
                 Assertions.assertTrue(orderList.getError().isEmpty(), "Error messages: " + orderList.getError());
-                if (orderList.getData().size() > 0){
-                    for (Order order:orderList.getData()) {
-                        try{
+
+                if (orderList.getData().size() > 0) {
+                    for (Order order : orderList.getData()) {
+
+                        try {
                             int id = Integer.parseInt(order.getId());
-                        }
-                        catch (NumberFormatException ex){
+                        } catch (NumberFormatException ex) {
                             System.out.println("Incorrect order_id: " + order.getId());
                         }
+
                         try {
-                            Assertions.assertEquals(ORDER_STATUS.values()[i].getOrderStatus(), order.getStatus(), "The order " +order.getId()+ " actual status: " + order.getStatus() + " Expected status: " + ORDER_STATUS.values()[i].getOrderStatus());
+                            Assertions.assertEquals(ORDER_STATUS.values()[i].getOrderStatus(), order.getStatus(), "The order " + order.getId() + " actual status: " + order.getStatus() + " Expected status: " + ORDER_STATUS.values()[i].getOrderStatus());
                             Assertions.assertFalse(order.getCreationDate().isEmpty(), "The order " + order.getId() + " creation date is empty");
                             Assertions.assertFalse(order.getDropoff().getPerson().getFirstName().isEmpty(), "The order " + order.getId() + " first name is empty");
-                            Assertions.assertTrue(order.getPayment().getTotal() >= 0 , "The order " + order.getId() + " total price is " + order.getPayment().getTotal());
+                            Assertions.assertTrue(order.getPayment().getTotal() >= 0, "The order " + order.getId() + " total price is " + order.getPayment().getTotal());
                             Assertions.assertFalse(order.getDropoff().getPerson().getPhone().isEmpty(), "The order " + order.getId() + " phone is empty");
-                            for (Order.Transaction transaction: order.getTransactions()) {
-                                if(transaction.getTransactionID() != null){
+
+                            for (Order.Transaction transaction : order.getTransactions()) {
+                                if (transaction.getTransactionID() != null) {
                                     Assertions.assertFalse(transaction.getTransactionID().isEmpty(), "The order " + order.getId() + " transactionID is empty. Type is " + transaction.getType() + " id : " + transaction.getId());
-                                }else {
+                                } else {
                                     System.out.println("The order " + order.getId() + " transactionID is NULL");
                                 }
                             }
-                        }catch (AssertionError e){
+                        } catch (AssertionError e) {
                             System.out.println(e.getMessage());
                         }
-
                     }
                     offset += limit;
-                }
-                else {
+                } else {
                     emptyData = true;
                 }
             }
