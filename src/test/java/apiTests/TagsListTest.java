ef5452850ba64e7ee363a2e60c4907a42a44e6f6
Oqsanna <oqsannas@localexpress.io>
2022-12-09 11:28:53 +0400

    Add Tags list

---
+++ src/test/java/apiTests/TagsListTest.java
@@ -0,0 +1,39 @@
+package apiTests;
+
+import com.google.gson.Gson;
+import io.restassured.http.Header;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import requests.TagsInfo;
+import responses.tags.SuccessCreateTags;
+import responses.tags.TagsList;
+import steps.data.users.UserInfoProvider;
+import storage.APIV1;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
+
+public class TagsListTest {
+    @Test
+    @DisplayName("check tags list")
+    public void getTagsListTest(){
+        Gson gson = new Gson();
+
+        TagsList tagsList = given()
+                .header(new Header("Authorization", "Bearer " + UserInfoProvider.getToken()))
+                .get(APIV1.STAGE.getApi() + APIV1.TAGSLIST.getApi())
+                .then().log().all()
+                .extract().as(TagsList.class);
+        Assertions.assertEquals("success", tagsList.getResult());
+        Assertions.assertEquals("", tagsList.getError());
+        Assertions.assertEquals(200, tagsList.getCode());
+        ArrayList<TagsInfo> tagsData = tagsList.getData();
+        for(TagsInfo data : tagsData){
+            Assertions.assertFalse(data.title.isEmpty(), "title  is empty");
+            Assertions.assertFalse(data.id.isEmpty(), "id  is empty");
+        }
+
+    }
+}
---
+++ src/test/java/responses/tags/TagsList.java
@@ -0,0 +1,14 @@
+package responses.tags;
+
+import requests.TagsInfo;
+import responses.tags.SuccessCreateTags;
+import lombok.Getter;
+
+import java.util.ArrayList;
+@Getter
+public class TagsList {
+    private Integer code;
+    private String result;
+    private  String error;
+    private ArrayList<TagsInfo> data;
+}
