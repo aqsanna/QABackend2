8d3aa5479c2cefa2a68bb12430ce95662c99a3d1
Oqsanna <oqsannas@localexpress.io>
2022-12-20 15:15:03 +0400

    Delete TagListTest class Add in TagsTest class getTagsListTest test

--- src/test/java/apiTests/TagsTest.java
+++ src/test/java/apiTests/TagsTest.java
@@ -7,11 +7,17 @@ import io.restassured.http.Header;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import requests.TagsInfo;
 import responses.tags.SuccessCreateTags;
+import responses.tags.TagsList;
 import steps.data.users.TagsInfoProvider;
 import steps.data.users.UserInfoProvider;
 import storage.APIV1;
 import storage.USER;
+
+import java.util.ArrayList;
+
+import static io.restassured.RestAssured.given;
 
 public class TagsTest {
     Gson gson = new Gson();
@@ -32,5 +38,26 @@ public class TagsTest {
         Assertions.assertEquals(200, successCreateTags.getCode());
         Assertions.assertFalse(successCreateTags.getData().getId().isEmpty(), "is  is empty");
         Assertions.assertFalse(successCreateTags.getData().getTitle().isEmpty(), "title  is empty");
+    }
+
+    @Test
+    @DisplayName("check tags list")
+    public void getTagsListTest(){
+        Gson gson = new Gson();
+
+        TagsList tagsList = given()
+                .header(new Header("Authorization", "Bearer " + UserInfoProvider.getToken()))
+                .get(APIV1.STAGE.getApi() + APIV1.TAGSLIST.getApi())
+                .then().log().all()
+                .extract().as(TagsList.class);
+        Assertions.assertEquals("success", tagsList.getResult());
+        Assertions.assertEquals("", tagsList.getError());
+        Assertions.assertEquals(200, tagsList.getCode());
+        ArrayList<TagsInfo> tagsData = tagsList.getData();
+        for(TagsInfo data : tagsData){
+            Assertions.assertFalse(data.title.isEmpty(), "title  is empty");
+            Assertions.assertFalse(data.id.isEmpty(), "id  is empty");
+            Assertions.assertEquals(tagsData.size(), 100);
+        }
     }
 }
