8c2e1cca775b33941a3e636d173f9843a4ed8361
hayk55dav <Hayk9710@gmail.com>
2023-02-24 11:09:21 +0400

    Collecting fixes

--- src/test/java/apiTests/CollectingOrderTest.java
+++ src/test/java/apiTests/CollectingOrderTest.java
@@ -11,6 +11,10 @@ import steps.data.order.CollectingOrderProvider;
 import storage.OrderStatus;
 import storage.User;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class CollectingOrderTest {
     @Test
     @DisplayName("Check the order collecting")
@@ -29,35 +33,41 @@ public class CollectingOrderTest {
         Order startOrder = orderUtils.postStartOrder(orderId, CollectingOrderProvider.orderStart(User.EMAIL_INFO));
         Assertions.assertEquals("assembling", startOrder.getData().getStatus(), "Order is not started collecting, The order status is" + startOrder.getData().getStatus());
 
-        for (Order.Data.GroupedItem groupedItem : startOrder.getData().getGroupedItems()) {
-            for (Order.Data.GroupedItem.OrderProduct orderProduct : groupedItem.getOrderProducts()) {
-                Order assembledProductOrder = orderUtils.postCollectOrderProduct(orderProduct.getId(), CollectingOrderProvider.collectOrderProduct(User.EMAIL_INFO));
-                Order.Data.GroupedItem.OrderProduct orderProductItem = assembledProductOrder.getData().getGroupedItems()
-                        .stream().flatMap(groupedItem1 -> groupedItem1.getOrderProducts()
-                                .stream().filter(orderProduct1 -> orderProduct1.getId().equals(orderProduct.getId())))
-                        .findAny()
-                        .orElse(null);
-                Assertions.assertEquals("assembled", orderProductItem.getStatus(), "The order product is not assembled, Order id: " + orderId + " orderProductId: " + orderProductItem.getId());
-            }
+        ArrayList<String> orderProductsIds = (ArrayList<String>) startOrder.getData().getGroupedItems().stream()
+                .flatMap(groupedItem -> groupedItem.orderProducts.stream())
+                .flatMap(orderProduct -> Stream.of(orderProduct.getId()))
+                .collect(Collectors.toList());
+        for (String orderProductId: orderProductsIds) {
+            Order assembledProductOrder = orderUtils.postCollectOrderProduct(orderProductId, CollectingOrderProvider.collectOrderProduct(User.EMAIL_INFO));
+            Order.Data.GroupedItem.OrderProduct orderProductItem = assembledProductOrder.getData().getGroupedItems()
+                    .stream().flatMap(groupedItem1 -> groupedItem1.getOrderProducts()
+                            .stream().filter(orderProduct1 -> orderProduct1.getId().equals(orderProductId)))
+                    .findAny()
+                    .orElse(null);
+            Assertions.assertEquals("assembled", orderProductItem.getStatus(), "The order product is not assembled, Order id: " + orderId + " orderProductId: " + orderProductItem.getId());
+
         }
 
-        if (startOrder.getData().getWrapping().isHasAdvancedCollectingFlow()) {
+        if (startOrder.getData().getWrapping().isHasAdvancedCollectingFlow()){
+            Assertions.assertNotEquals(0, startOrder.getData().getWrapping().getPacks().size(), "The store have not packs");
             String packId = startOrder.getData().getWrapping().getPacks().get(0).getId();
             float price = Float.parseFloat(startOrder.getData().getWrapping().getPacks().get(0).getPrice());
-            for (Order.Data.Wrapping.Pack pack : startOrder.getData().getWrapping().getPacks()) {
-                if (Integer.parseInt(pack.getFreeQty()) > 0) {
+            for(Order.Data.Wrapping.Pack pack : startOrder.getData().getWrapping().getPacks()){
+                if(Integer.parseInt(pack.getFreeQty()) > 0){
                     packId = pack.getId();
                     break;
-                } else if (Float.parseFloat(pack.getPrice()) <= price) {
+                }
+                else if (Float.parseFloat(pack.getPrice()) <= price) {
                     packId = pack.getId();
                 }
             }
-            orderUtils.postAddPacksToOrder(orderId, AddPacksToOrderProvider.addPack(User.EMAIL_INFO, packId, "1"));
+            String productQty = "1";
+            String cloudPrinterId = "2";
+            orderUtils.postAddPacksToOrder(orderId, AddPacksToOrderProvider.addPack(User.EMAIL_INFO, packId, productQty));
             orderUtils.postAddPackLocation(orderId, AddPacksToOrderProvider.addPackLocations(User.EMAIL_INFO, null, packId, ""));
-            orderUtils.postPrintPackLocation(orderId, AddPacksToOrderProvider.printPackLocation(User.EMAIL_INFO, "2"));
+            orderUtils.postPrintPackLocation(orderId, AddPacksToOrderProvider.printPackLocation(User.EMAIL_INFO, cloudPrinterId));
         }
 
         orderUtils.postFinishOrder(orderId, CollectingOrderProvider.orderConfirm(User.EMAIL_INFO));
-
     }
 }
